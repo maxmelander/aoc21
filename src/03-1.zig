4616a0bdc80090de18f7ffcb03db55b6de890547
Max Melander <mail@maxmelander.com>
2021-12-03 13:25:39 +0100

    No need to count both 0s and 1s

--- src/03-1.zig
+++ src/03-1.zig
@@ -2,27 +2,18 @@ const std = @import("std");
 const print = std.debug.print;
 const file = @embedFile("../input/03.txt");
 
-const Counter = struct {
-    num_0: usize = 0,
-    num_1: usize = 0
-};
-
 pub fn main() !void {
     const timer = try std.time.Timer.start();
     var it = std.mem.tokenize(u8, file, "\n");
 
     // Count bits
-    var arr: [12]Counter = .{Counter{}} ** 12;
+    var bit_counts: [12]usize = .{0} ** 12;
     while (it.next()) |line| {
         const number = try std.fmt.parseUnsigned(u12, line, 2);
         var bit_index: u4 = 0;
         while (bit_index < 12) : (bit_index += 1) {
             const bit = (number >> (bit_index)) & 1;
-            if (bit == 0) {
-                arr[bit_index].num_0 += 1;
-            } else {
-                arr[bit_index].num_1 += 1;
-            }
+            bit_counts[bit_index] += 1 * bit;
         }
     }
 
@@ -31,9 +22,9 @@ pub fn main() !void {
     var epsilon: u64 = 0;
     var result: u64 = 0;
     var i: u4 = 0;
-    for (arr) |count| {
-        const gamma_bit: u12 = if (count.num_0 > count.num_1) 0 else 1;
-        const epsilon_bit: u12 = if (count.num_0 > count.num_1) 1 else 0;
+    for (bit_counts) |count| {
+        const gamma_bit: u12 = if (count > 500) 0 else 1;
+        const epsilon_bit: u12 = if (count > 500) 1 else 0;
         gamma |= (gamma_bit << i);
         epsilon |= (epsilon_bit << i);
         i += 1;
