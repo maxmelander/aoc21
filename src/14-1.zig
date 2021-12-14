bde74f0fb8bf459fce43b3f5bd3dddc6e1d24ed6
Max Melander <mail@maxmelander.com>
2021-12-14 20:21:06 +0100

    day 14

--- src/14-1.zig
+++ src/14-1.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const print = std.debug.print;
-const file = @embedFile("../input/14-test.txt");
+const file = @embedFile("../input/14.txt");
 
 const HashMap = std.StringHashMap(usize);
 const FnMap = std.StringHashMap(Thingy);
@@ -10,15 +10,16 @@ const Thingy = struct {
     wedge: []const u8,
 
     const Self = @This();
-    pub fn do(self: *Self, counts: *HashMap) void {
+    pub fn do(self: *Self, counts: *HashMap, totals: *HashMap, count: usize) void {
+        totals.getPtr(self.wedge).?.* += count;
         if (counts.getPtr(self.pair)) |counts_ptr| {
-            counts_ptr.* -= 1;
+            counts_ptr.* -= count;
         }
         if (counts.getPtr(&.{ self.pair[0], self.wedge[0] })) |counts_ptr| {
-            counts_ptr.* += 1;
+            counts_ptr.* += count;
         }
         if (counts.getPtr(&.{ self.wedge[0], self.pair[1] })) |counts_ptr| {
-            counts_ptr.* += 1;
+            counts_ptr.* += count;
         }
     }
 };
@@ -41,7 +42,19 @@ pub fn main() !void {
         } else {
             try counts.putNoClobber(input[i .. i + 2], 1);
         }
+
+        if (totals.getPtr(input[i .. i+1])) |totals_ptr| {
+            totals_ptr.* += 1;
+        } else {
+            try totals.putNoClobber(input[i .. i + 1], 1);
+        }
     }
+    if (totals.getPtr(input[i .. i+1])) |totals_ptr| {
+        totals_ptr.* += 1;
+    } else {
+        try totals.putNoClobber(input[i .. i + 1], 1);
+    }
+
 
     // Build mapping thingy
     while (it.next()) |line| {
@@ -55,48 +68,53 @@ pub fn main() !void {
         }
 
         // Fill up our totals map
-        try totals.put(left[0..1], 0);
-        try totals.put(left[1..2], 0);
+        if (totals.getPtr(left[0..1])) |_| {} else {
+            try totals.put(left[0..1], 0);
+        }
+
+        if (totals.getPtr(left[1..2])) |_| {} else {
+            try totals.put(left[1..2], 0);
+        }
     }
 
     // Run the steps
     var step: usize = 0;
-    while (step < 10) : (step += 1) {
-        print("hello\n", .{});
+    while (step < 40) : (step += 1) {
+        var key_it = counts.keyIterator();
         var clone = try counts.clone();
-        var key_it = clone.keyIterator();
         while (key_it.next()) |key| {
-            if (clone.get(key.*).? == 0) continue;
+            const counts_value = clone.get(key.*).?;
+            if (counts_value == 0) continue;
 
-            print("KEY {s}\n", .{key.*});
+
             if (map.getPtr(key.*)) |map_thingy| {
-                map_thingy.*.do(&counts);
+                map_thingy.*.do(&counts, &totals, counts_value);
             }
         }
-    }
-
-    // Count up all the individual chars
-    var key_it = counts.keyIterator();
-    while (key_it.next()) |key| {
-        if (totals.getPtr(key.*[0..1])) |totals_ptr| {
-            totals_ptr.* += counts.get(key.*).?;
-        }
-        if (totals.getPtr(key.*[1..2])) |totals_ptr| {
-            totals_ptr.* += counts.get(key.*).?;
-        }
+        //counts = clone;
     }
 
     // Find the highest value
-    key_it = totals.keyIterator();
+    var key_it = totals.keyIterator();
     var largest: usize = 0;
+    var smallest: usize = 99999999999999;
     var largestKey: []const u8 = "";
+    var smallestKey: []const u8 = "";
     while (key_it.next()) |key| {
         const value = totals.get(key.*).?;
         if (value > largest) {
             largest = value;
             largestKey = key.*;
         }
+
+        if (value < smallest) {
+            smallest = value;
+            smallestKey = key.*;
+        }
     }
 
     print("largest: {s} {}\n", .{ largestKey, largest });
+    print("smallest: {s} {}\n", .{ smallestKey, smallest });
+    print("result: {}", .{largest - smallest});
+    //print("length: {}\n", .{length});
 }
